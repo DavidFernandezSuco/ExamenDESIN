5d4d740fd57023a8c5395e31a2dbdbb816e5616c
David Suco <dfsuco@gmail.com>
2025-03-10 10:43:32 +0100

    añadir interfaz

--- src/main/java/main/MainInterfaz.java
+++ src/main/java/main/MainInterfaz.java
@@ -3,6 +3,9 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class MainInterfaz {
 
@@ -38,6 +41,16 @@ public class MainInterfaz {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Perdón por la tarea de PMDM");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(31, 36, 317, 64);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Regalar un 5");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnNewButton.setBounds(104, 121, 186, 64);
+		frame.getContentPane().add(btnNewButton);
 	}
-
 }
